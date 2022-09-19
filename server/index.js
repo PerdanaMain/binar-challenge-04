728e9244a7733eb1607fda5b4f2b47d28871a295
Perdana Main <firman.fp123@gmail.com>
2022-09-19 23:49:56 +0700

    Memperbaiki server dan install module nodemon

--- server/index.js
+++ server/index.js
@@ -1,38 +1,29 @@
-// console.log("Implement servermu disini yak 😝!");
-
 // call modules
 const fs = require("fs");
 const http = require("http");
 
 const port = 3000;
 
-const renderHtml = (path, res) => {
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      res.writeHead(404);
-      res.write("Error: File Not Found");
-    } else {
-      res.write(data);
-    }
-    res.end();
-  });
-};
-
 http
   .createServer((req, res) => {
-    res.writeHead(200, {
-      "Content-type": "text/html",
-    });
     const url = req.url;
     switch (url) {
       case "/search-car":
-        renderHtml("../public/search-car.html", res);
+        fs.readFile("../public/search-car.html", (err, html) => {
+          res.writeHead(200, {
+            "Content-type": "text/html",
+          });
+        });
         break;
       default:
-        renderHtml("../public/index.html", res);
+        fs.readFile("../public/index.html", (err, html) => {
+          res.writeHead(200, {
+            "Content-type": "text/html",
+          });
+        });
         break;
     }
   })
   .listen(port, () => {
-    console.log(`Server is Listening On Port ${port}`);
+    console.log(`Server is Listening On http://localhost:${port}`);
   });
