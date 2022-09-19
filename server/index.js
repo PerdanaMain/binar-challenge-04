c236f47ac2aa9c5dc552104cb9e38c485aefae96
Perdana Main <firman.fp123@gmail.com>
2022-09-19 11:02:31 +0700

    Membuat Server dan Render HTML pada server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,38 @@
-console.log("Implement servermu disini yak 😝!");
+// console.log("Implement servermu disini yak 😝!");
+
+// call modules
+const fs = require("fs");
+const http = require("http");
+
+const port = 3000;
+
+const renderHtml = (path, res) => {
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.write("Error: File Not Found");
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+};
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    const url = req.url;
+    switch (url) {
+      case "/search-car":
+        renderHtml("../public/search-car.html", res);
+        break;
+      default:
+        renderHtml("../public/index.html", res);
+        break;
+    }
+  })
+  .listen(port, () => {
+    console.log(`Server is Listening On Port ${port}`);
+  });
