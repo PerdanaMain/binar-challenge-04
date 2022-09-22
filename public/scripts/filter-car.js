664e52b96d8e7dbf512319de11bad10901d6d134
Perdana Main <firman.fp123@gmail.com>
2022-09-22 11:37:57 +0700

    Load json di web server

--- public/scripts/filter-car.js
+++ public/scripts/filter-car.js
@@ -2,18 +2,30 @@ class Car {
   constructor(cars) {
     this.cars = cars;
   }
-  filterCarAvailable() {
-    return this.cars.filter((mobil) => mobil.available === true);
+  filterCarAvailable(cars) {
+    const result = [];
+    for (let i = 0; i < cars.length; i++) {
+      if (cars[i].available) {
+        result.push(cars[i]);
+      }
+    }
+    return result;
   }
-  filterCarNotAvailable() {
-    return this.cars.filter((mobil) => mobil.available === false);
+  filterCarNotAvailable(cars) {
+    const result = [];
+    for (let i = 0; i < cars.length; i++) {
+      if (cars[i].available === false) {
+        result.push(cars[i]);
+      }
+    }
+    return result;
   }
   filterCarBySelected() {
     let driver = document.getElementById("driver").value;
-    let date = document.getElementById("date").value;
-    let time = document.getElementById("time").value;
+    let date = document.getElementById("tanggal").value;
+    let time = document.getElementById("jemput").value;
     let dateTime = date + time;
-    var capacity = document.getElementById("capacity").value;
+    var capacity = document.getElementById("jmlPenumpang").value;
 
     if (driver === undefined || driver == "") {
       alert("Please select a driver");
@@ -22,29 +34,86 @@ class Car {
     } else if (driver.value === "tanpa-supir") {
       return this.filterCarNotAvailable(this.cars);
     } else if (dateTime === undefined || dateTime === "") {
-      alert("Please select the date");
-    } else if (capacity == "" && driver.toString() == "true") {
-      return this.cars.filter(
-        (mobil) => mobil.available === true && mobil.availableAt <= dateTime
-      );
-    } else if (capacity != "" && driver.toString() == "true") {
+      alert("Please select Date and Time");
+    } else if (capacity == "" && driver.value == "dengan-supir") {
+      return this.cars.filter((mobil) => {
+        mobil.available === true && mobil.availableAt <= dateTime;
+      });
+    } else if (capacity != "" && driver.value == "dengan-supir") {
       return this.cars.filter(
         (mobil) =>
           mobil.available === true &&
           mobil.capacity >= capacity &&
           mobil.availableAt <= dateTime
       );
-    } else if (capacity == "" && driver.toString() == "false") {
+    } else if (capacity == "" && driver.value == "tanpa-supir") {
       return this.cars.filter(
         (mobil) => mobil.available === false && mobil.availableAt <= dateTime
       );
-    } else if (capacity != "" && driver.toString() == "false") {
+    } else if (capacity != "" && driver.value == "tanpa-supir") {
       return this.cars.filter(
         (mobil) =>
           mobil.available === false &&
           mobil.capacity >= capacity &&
           mobil.availableAt <= dateTime
       );
+    }
+  }
+}
+
+let btnFilterCar = document
+  .getElementById("btnFilterCar")
+  .addEventListener("click", getCars);
+
+function getDateTimeNow() {
+  let today = new Date();
+  let date =
+    today.getFullYear() +
+    "-" +
+    String(today.getMonth() + 1).padStart(2, "0") +
+    "-" +
+    String(today.getDate()).padStart(2, "0");
+  let time =
+    String(today.getHours()).padStart(2, "0") +
+    ":" +
+    String(today.getMinutes()).padStart(2, "0") +
+    ":" +
+    String(today.getSeconds()).padStart(2, "0");
+  let dateNow = date + "T" + time + ".000Z";
+  return dateNow;
+}
+
+function getCars() {
+  let htmlData = "";
+  data = cars.filtermobil();
+  if (data === "" || data === undefined) {
+    htmlData = "";
+    app.innerHTML = htmlData;
+    return;
+  } else {
+    for (let index = 0; index < data.length; index++) {
+      let car = data[index];
+
+      htmlData += `
+            <div class="col m-2">
+                <div class="card" style="width: 20rem; height: 450px">
+                <img src="${car.image}"" class="img-fluid card-img-top " alt="${car.manufacture}" style="object-fit: scale-down; height: 200px; ">
+                <div class="card-body" style="font-size: 14px;">
+                    <p class="card-title">${car.manufacture} ${car.model}</p>
+                    <p class="fw-bold">Rp. ${car.rentPerDay} / hari</p>
+                    <p class="card-text" style="height: 85px">${car.description}</p>
+                    <div class=""><i class="bi bi-people me-2"></i>${car.capacity} Orang</div>
+                    <div class=""><i class="bi bi-gear me-2"></i>${car.transmission}</div>
+                    <div class=""><i class="bi bi-calendar4 me-2"></i>${car.year}</div>
+                    <a href="#" class="btn bg-button text-white w-100 mt-2 fw-bold mt-4" style="font-size: 14px;">Pilih Mobil</a>
+                </div>
+                </div>
+            </div>
+            `;
+    }
+    app.innerHTML = htmlData;
+    if (htmlData == "") {
+      alert("No car available");
     }
   }
 }
--- server/index.js
+++ server/index.js
@@ -15,7 +15,9 @@ http
         (err, data) => {
           if (err) {
             res.writeHead(404);
-            res.write("Error: File Not Found");
+            res.write(
+              "<center style='margin-top: 100px ' ><h2>404: File Not Found</h2></center>"
+            );
           } else {
             res.write(data);
           }
@@ -29,7 +31,9 @@ http
         (err, data) => {
           if (err) {
             res.writeHead(404);
-            res.write("Error: File Not Found");
+            res.write(
+              "<center style='margin-top: 100px ' ><h2>404: File Not Found</h2></center>"
+            );
           } else {
             res.write(data);
           }
@@ -38,21 +42,34 @@ http
       );
     } else if (req.url.match(".css$")) {
       const cssPath = path.join("./public", req.url);
-      const fileStream = fs.createReadStream(cssPath, "UTF-8");
+      const fileStream = fs.createReadStream(cssPath);
       res.writeHead(200, { "Content-Type": "text/css" });
       fileStream.pipe(res);
     } else if (req.url.match(".js$")) {
       const jsPath = path.join("./public", req.url);
-      const fileStream = fs.createReadStream(jsPath, "UTF-8");
-      res.writeHead(200, { "Content-Type": "text/css" });
+      const fileStream = fs.createReadStream(jsPath);
+      res.writeHead(200, { "Content-Type": "application/javascript" });
       fileStream.pipe(res);
     } else if (req.url.match(".png$")) {
       const imagePath = path.join("./public", req.url);
       const fileStream = fs.createReadStream(imagePath);
       res.writeHead(200, { "Content-Type": "image/png" });
       fileStream.pipe(res);
+    } else if (req.url === "/getcars") {
+      const dataPath = path.join(__dirname, "../data", "/cars.json");
+      const fileStream = fs.createReadStream(dataPath, "UTF-8");
+      res.writeHead(200, { "Content-Type": "application/json" });
+      fileStream.pipe(res);
+    } else if (req.url === "/api/cars") {
+      res.setHeader("Content-Type", "application/json");
+      res.writeHead(200);
+      res.end(JSON.stringify(cars));
+    } else {
+      res.writeHead(404);
+      res.write("<h1>404: File Not Found</h1>");
+      res.end();
     }
   })
   .listen(port, () => {
-    console.log(`Server is listening on http://localhost:${port}`);
+    console.log(`Server is listening at http://localhost:${port}`);
   });
