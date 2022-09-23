2690e9abe3ce9ba724633c8b4b451a32682fc158
Perdana Main <firman.fp123@gmail.com>
2022-09-23 20:43:05 +0700

    Memperbaiki alert jika data not found

--- public/scripts/filter-car.js
+++ public/scripts/filter-car.js
@@ -12,7 +12,7 @@ class Car {
     if (driver == "" || driver == undefined) {
       alert("Please Select Driver Type");
     } else if (date == "" || date == undefined) {
-      alert("Please Select the date");
+      alert("Please Select the Date");
     } else if (time == "" || time == undefined) {
       alert("Please Select The Time");
     } else if (driver == "dengan-supir") {
@@ -47,28 +47,26 @@ class Car {
 
 let xmlHttp = new XMLHttpRequest();
 xmlHttp.open("GET", "http://localhost:3000/cars", false);
-xmlHttp.send(null); // Request body null
-
+xmlHttp.send(); // Request body null
 
 // Initiate obj Car
 let cars = new Car(JSON.parse(xmlHttp.responseText));
 
-// Ambil id carsList
-let app = document.getElementById("carsList");
-
 let btnSearch = document
   .getElementById("btn-search")
-  .addEventListener("click", ()=>{
-  let text = "";
-  let data = cars.filterCarBySelected();
-  if (data === "" || data === undefined) {
-    text = "";
-    app.innerHTML = text;
-    return;
-  } else {
-    for (let index = 0; index < data.length; index++) {
-      let car = data[index];
-      text += `
+  .addEventListener("click", () => {
+    // Ambil id carsList
+    let list = document.getElementById("carsList");
+    let text = "";
+    let data = cars.filterCarBySelected();
+    if (data === "" || data === undefined) {
+      text = "Data Belum di Initiate";
+      list.innerHTML = text;
+      return;
+    } else {
+      for (let index = 0; index < data.length; index++) {
+        let car = data[index];
+        text += `
       <div class="col-md">
         <div class="card" style="width: 18rem;">
           <img src="${car.image}" class="card-img-top" alt="${car.manufacture}">
@@ -76,18 +74,22 @@ let btnSearch = document
             <h5 class="card-title">${car.manufacture} ${car.model}</h5>
             <p class="fst fst-bold">Rp. ${car.rentPerDay} / hari</p>
             <p class="card-text">${car.description}</p>
-            <div class=""><i class="me-2"></i>${car.capacity} Orang</div>
-            <div class=""><i class="me-2"></i>${car.transmission}</div>
-            <div class=""><i class="me-2"></i>${car.year}</div>
+            <div class="">${car.capacity} Orang</div>
+            <div class="">${car.transmission}</div>
+            <div class="">${car.year}</div>
             <a href="" class="btn btn-success w-100 mt-2 fw-bold" style="font-size: 14px;">Pilih Mobil</a>
           </div>
         </div>
       </div>
       `;
+      }
+      list.innerHTML = text;
+      if (text == "") {
+        document.getElementById("notMatch").innerHTML = `
+        <div class="alert alert-danger " style="padding: 10px 10px 10px 10px; margin-left: auto; margin-right: auto; margin-top: -80px; width: 50%;" role="alert">
+          No Car Found!
+        </div>
+        `;
+      }
     }
-    app.innerHTML = text;
-    if (text == "") {
-      document.getElementById("notMatch").innerHTML = "No Car Found!"
-    }
-  }
   });
